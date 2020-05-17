8b8d56f8b5bd7325a4c9fa6f57f57172064de476
Marcelo-RM <marcelordrgs98@gmail.com>
2020-05-17 16:33:17 -0300

    Updates

---
+++ precache-manifest.43c9d43159cacdf5fa4be14caed4236e.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "795c6dbd7c7873046c19965f0067769c",
+    "url": "/duolingo_copy/index.html"
+  },
+  {
+    "revision": "0beaa91bc498a28ce248",
+    "url": "/duolingo_copy/static/css/main.aecc4508.chunk.css"
+  },
+  {
+    "revision": "d0234ff08d5bb33e20a0",
+    "url": "/duolingo_copy/static/js/2.ecc8880a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/duolingo_copy/static/js/2.ecc8880a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0beaa91bc498a28ce248",
+    "url": "/duolingo_copy/static/js/main.5b2e7dd1.chunk.js"
+  },
+  {
+    "revision": "1e1afa384106234c708a",
+    "url": "/duolingo_copy/static/js/runtime-main.6d9b5b4e.js"
+  },
+  {
+    "revision": "7ba2c9ee70a2a17547eb9cc4d0306c16",
+    "url": "/duolingo_copy/static/media/add-course.7ba2c9ee.svg"
+  },
+  {
+    "revision": "d078166cbc81ac85c4dcce01946c8177",
+    "url": "/duolingo_copy/static/media/crown.d078166c.svg"
+  },
+  {
+    "revision": "6b9017b5b326c9949ffb007c8cbd7f4c",
+    "url": "/duolingo_copy/static/media/discuss-selected.6b9017b5.svg"
+  },
+  {
+    "revision": "806b1367b271436244ad7f4dc7f3da5f",
+    "url": "/duolingo_copy/static/media/discuss.806b1367.svg"
+  },
+  {
+    "revision": "9acefd83e2fe308467d71711b84780fa",
+    "url": "/duolingo_copy/static/media/duo-traveler.9acefd83.svg"
+  },
+  {
+    "revision": "54204a9c3c2f55d59b7217a4ab40a22c",
+    "url": "/duolingo_copy/static/media/fire.54204a9c.svg"
+  },
+  {
+    "revision": "52dbf39468195b7c95c0504252738b9d",
+    "url": "/duolingo_copy/static/media/flags.52dbf394.svg"
+  },
+  {
+    "revision": "83a2d147812864a32496646992fc5994",
+    "url": "/duolingo_copy/static/media/gem.83a2d147.svg"
+  },
+  {
+    "revision": "df4eddac70868479a8468f6258553cbf",
+    "url": "/duolingo_copy/static/media/learn-selected.df4eddac.svg"
+  },
+  {
+    "revision": "b3e4bff7190c6506d58b5a5d86780b0e",
+    "url": "/duolingo_copy/static/media/learn.b3e4bff7.svg"
+  },
+  {
+    "revision": "67f03a5e9cb2cfa3b19e11fe9fd63eb7",
+    "url": "/duolingo_copy/static/media/profile-selected.67f03a5e.svg"
+  },
+  {
+    "revision": "4a4dab474e8724b4a26230d8a5492cc7",
+    "url": "/duolingo_copy/static/media/profile.4a4dab47.svg"
+  },
+  {
+    "revision": "3bdf2f3024c1dd916fedfbd1e92f2d2f",
+    "url": "/duolingo_copy/static/media/sad.3bdf2f30.svg"
+  },
+  {
+    "revision": "0ee420c222c8351903f2a0d2562d22d0",
+    "url": "/duolingo_copy/static/media/store-selected.0ee420c2.svg"
+  },
+  {
+    "revision": "bdbab8a777f33ed31f019b22098e784e",
+    "url": "/duolingo_copy/static/media/store.bdbab8a7.svg"
+  },
+  {
+    "revision": "47d85c5b0a0965a0d6595a21bf29b443",
+    "url": "/duolingo_copy/static/media/stories-selected.47d85c5b.svg"
+  },
+  {
+    "revision": "034d6aefec344d9065aa2e3393e4fede",
+    "url": "/duolingo_copy/static/media/stories.034d6aef.svg"
+  },
+  {
+    "revision": "dcca49d7bdd6998c159eb0b28aa8659b",
+    "url": "/duolingo_copy/static/media/train.dcca49d7.svg"
+  }
+]);
